31437d9bd5d1a8dbeca70fe72d2bcc4aeb0aa41c
plaski <maciejploskonka@gmail.com>
2017-03-10 16:18:52 +0100

    troche dziala

--- scripts.js
+++ scripts.js
@@ -17,8 +17,7 @@ function moveFirstSlide() {
 
 $('#getNextSlide').click(function() {
 	carouselList.animate({'marginLeft':-500}, 1000, moveFirstSlide);
-	clearInterval(timer);
-	timer = setInterval(changeSlide, 5000);
+	resetInterval();
 });
 
 $('#getPrevSlide').click(function() {
@@ -27,32 +26,32 @@ $('#getPrevSlide').click(function() {
 	firstItem.before(lastItem);
 	carouselList.css({marginLeft:-500});
 	carouselList.animate({'marginLeft':0}, 1000);
+	resetInterval();
+});
+
+function resetInterval() {
 	clearInterval(timer);
 	timer = setInterval(changeSlide, 5000);
-});
+};
+
+
 $('#getFirstSlide').click(function() {
 	var index = $('#firstSlide').index();
-	console.log(index);
-	carouselList.animate({'marginLeft':-500*index}, 500);
+	carouselList.animate({'marginLeft':-500*index}, 500);	
 });
 $('#getSecondSlide').click(function() {
 	var index = $('#secondSlide').index();
-	console.log(index);
 	carouselList.animate({'marginLeft':-500*index}, 500);
-	
 });
 $('#getThirdSlide').click(function() {
-	var index = $('#thirdSlide').index();
-	console.log(index);
+	var index = $('#thirdSlide').index()
 	carouselList.animate({'marginLeft':-500*index}, 500);
 });
 $('#getFourthSlide').click(function() {
 	var index = $('#fourthSlide').index();
-	console.log(index);
 	carouselList.animate({'marginLeft':-500*index}, 500);
 });
 $('#getFifthSlide').click(function() {
 	var index = $('#fifthSlide').index();
-	console.log(index);
 	carouselList.animate({'marginLeft':-500*index}, 500);
-})
+});
